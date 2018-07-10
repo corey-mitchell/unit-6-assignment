748b4e58ff1e307a0d2925e677d99a0ed5d16e12
Macaroni Biff <corey.waitforit.mitchell@gmail.com>
2018-07-09 22:53:48 -0500

    got a good start on JS, commiting and pushing for the night.

--- assets/Javascript/ajax.js
+++ assets/Javascript/ajax.js
@@ -1,3 +1,29 @@
 // Initial Variables
+var topics = ["topic1", "topic2", "topic3", "topic4", "topic5"];
+var userTopics = $("#topicInput")
 
-var topics = [];
+// Document Ready
+$(document).ready(function () {
+    // for loop for creating initial topic buttons
+    for (i = 0; i < topics.length; i++) {
+        $("#topicButtons").append("<button class='topicButtons'>" + topics[i] + "</button>")
+    }
+
+
+    //activates submit button
+    $("#submitButton").click(userInput)
+
+    // controls submit button
+    function userInput (e) {
+        e.preventDefault();
+
+        // keeps users from being able to add multiple of the same topic
+        if (topics.indexOf(userTopics.val()) === -1) {
+            topics.push(userTopics.val())
+            $("#topicButtons").append("<button class='topicButtons'>" + userTopics.val() + "</button>")
+            console.log(topics)
+        }
+
+    }
+
+});
