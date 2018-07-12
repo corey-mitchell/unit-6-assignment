fde3b9b205d303d58664fbe2ddbc5f89966e3cd5
Macaroni Biff <corey.waitforit.mitchell@gmail.com>
2018-07-11 19:36:19 -0500

    connected api to topic buttons to console.log responses

--- assets/Javascript/ajax.js
+++ assets/Javascript/ajax.js
@@ -1,29 +1,48 @@
 // Initial Variables
-var topics = ["topic1", "topic2", "topic3", "topic4", "topic5"];
+var topics = ["batman", "superman", "spiderman", "john wayne"];
 var userTopics = $("#topicInput")
+
 
 // Document Ready
 $(document).ready(function () {
-    // for loop for creating initial topic buttons
+    // Loop for Creating Inital Topic Buttons
     for (i = 0; i < topics.length; i++) {
-        $("#topicButtons").append("<button class='topicButtons'>" + topics[i] + "</button>")
+        // console.log(topics)
+        $("#topicButtons").append(`<button id="${topics[i]}" class="topicButtons">${topics[i]}</button>`)
     }
 
 
-    //activates submit button
+    //Activates Submit Button
     $("#submitButton").click(userInput)
 
-    // controls submit button
+    // Submit Button Function
     function userInput (e) {
         e.preventDefault();
 
-        // keeps users from being able to add multiple of the same topic
+        // Keeps Users From Being Able to Add Multiple of the Same Topic
         if (topics.indexOf(userTopics.val()) === -1) {
             topics.push(userTopics.val())
-            $("#topicButtons").append("<button class='topicButtons'>" + userTopics.val() + "</button>")
-            console.log(topics)
+            $("#topicButtons").append(`<button id="${userTopics.val()}" class="topicButtons">${userTopics.val()}</button>`)
+            // console.log(topics)
         }
 
     }
+
+
+    // Activates and Controls Topic Buttons
+    $(document).on('click', '.topicButtons', function(event) {
+        console.log(event.target.id)
+
+        // Places User Click Into Giphy API
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=AttVip230BugV7EkTVWyqtcdLXTohkOw&q=" + event.target.id + "&limit=10&offset=0&rating=PG&lang=en"
+
+        // API Function
+        $.get(queryURL).then(function(response) {
+            console.log(response)
+
+        })
+
+    })
+
 
 });
